700c76ad88bcfb28ca6b405d3546c8faf040684f
TT <83678908+B0BA-T3A-Coding@users.noreply.github.com>
2022-01-24 18:43:02 -0500

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyClAycNArvgM2cE7_n2ZNGZShUfM7bTZe4",
+    authDomain: "p-94-68f59.firebaseapp.com",
+    projectId: "p-94-68f59",
+    storageBucket: "p-94-68f59.appspot.com",
+    messagingSenderId: "57708304619",
+    appId: "1:57708304619:web:e29a5d977c55b5fb8986ae"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
